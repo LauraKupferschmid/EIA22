4909be92aa27468b7b96b08dca2281f28849a981
LauraKupferschmid <116755408+LauraKupferschmid@users.noreply.github.com>
2022-11-20 19:22:28 +0100

    Update main.js

--- Aufgabe_6/main.js
+++ Aufgabe_6/main.js
@@ -5,10 +5,10 @@ const Frankreich = 67.50;
 const Italien = 59.07;
 const Schweden = 10.42;
 const Europa = 447.70;
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise" + Deutschland + " Mio .");
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise" + Frankreich + " Mio .");
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise" + Italien + " Mio .");
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise" + Schweden + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise " + Deutschland + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise " + Frankreich + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise " + Italien + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise " + Schweden + " Mio .");
 console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2022 schätzungsweise " + Europa + " Mio.");
 //Einwohnergesamtanzahl 2008
 const Deutschland08 = 82.11;
@@ -16,11 +16,11 @@ const Frankreich08 = 64.37;
 const Italien08 = 58.83;
 const Schweden08 = 9.22;
 const Europa08 = 440.66;
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise" + Deutschland08 + " Mio .");
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise" + Frankreich08 + " Mio .");
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise" + Italien08 + " Mio .");
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise" + Schweden08 + " Mio .");
-console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise" + Europa08 + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise " + Deutschland08 + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise " + Frankreich08 + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise " + Italien08 + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise " + Schweden08 + " Mio .");
+console.log("Die Einwohner Zahl von Deutschand betrug im Jahr 2008 schätzungsweise " + Europa08 + " Mio .");
 //Wachstum der Länder von 2008 bis 2022
 //Deutschland
 const DifferenzDT = Deutschland - Deutschland08;
